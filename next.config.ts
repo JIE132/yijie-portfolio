c7d0a46cc3a781daa1278dcc614897baa28a3dd2
JIE132 <nb213426686@163.com>
2026-04-20 07:31:05 +0800

    add netlify configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 关键：告诉 Next.js 将整个项目生成 HTML/CSS/JS 静态文件
+  images: {
+    unoptimized: true, // 静态导出必须关闭图片优化
+  },
 };
 
 export default nextConfig;
